e10a8600a19f42078aef0241ba9e0b283ebd1606
Kelbin <kaeltero@gmail.com>
2021-03-27 23:20:58 +0300

    For Lexa

---
+++ MetropolLifeiOS/MetropolLifeiOS/Scenes/Main/Cells/ItemsCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ItemsCollectionViewCell.swift
+//  MetropolLifeiOS
+//
+//  Created by Kelbinary on 27.03.2021.
+//  Copyright © 2021 Kelbinary. All rights reserved.
+//
+
+import Foundation
